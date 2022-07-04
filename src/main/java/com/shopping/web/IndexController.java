01ea37e8ed04e023e1f80f7308933fa050cace1b
백준형 <gjanehrm1@gmail.com>
2022-07-04 16:48:29 +0900

    타일즈에서 타임리프로 변경

--- src/main/java/com/shopping/web/IndexController.java
+++ src/main/java/com/shopping/web/IndexController.java
@@ -1,38 +1,51 @@
 package com.shopping.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
 
-    @GetMapping("/")
-    public String moveIndex() {
-        return "index.tiles";
+    @GetMapping("")
+    public ModelAndView moveIndex() {
+    	ModelAndView mv = new ModelAndView();
+    	mv.setViewName("/index");
+        return mv;
     }
     
     @GetMapping("/index")
-    public String moveHome() {
-        return "index.tiles";
+    public ModelAndView moveHome() {
+    	ModelAndView mv = new ModelAndView();
+    	mv.setViewName("common/layout");
+        return mv;
     }
     
     @GetMapping("/about")
-    public String moveAbout() {
-    	return "about.tiles";
+    public ModelAndView moveAbout() {
+    	ModelAndView mv = new ModelAndView();
+    	mv.setViewName("/about");
+        return mv;
     }
     
     @GetMapping("/shop")
-    public String moveShop() {
-    	return "shop.tiles";
+    public ModelAndView moveShop() {
+    	ModelAndView mv = new ModelAndView();
+    	mv.setViewName("/shop");
+        return mv;
     }
     
     @GetMapping("/blog")
-    public String moveBlog() {
-    	return "blog.tiles";
+    public ModelAndView moveBlog() {
+    	ModelAndView mv = new ModelAndView();
+    	mv.setViewName("/blog");
+        return mv;
     }
     
     @GetMapping("/contact")
-    public String moveContact() {
-    	return "contact.tiles";
+    public ModelAndView moveContact() {
+    	ModelAndView mv = new ModelAndView();
+    	mv.setViewName("/contact");
+        return mv;
     }
     
 }
